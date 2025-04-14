ee53ae2b9b5c7cfb45cb7ee3414d743b7a345753
Akram <khalilovakram07@gmail.com>
2025-04-14 20:22:59 +0600

    new changes

--- src/components/CourseItem/CourseItem.jsx
+++ src/components/CourseItem/CourseItem.jsx
@@ -7,6 +7,8 @@ const CourseItem = (props) => {
             <h4>Title: {props.title}</h4>
             <h4>Duration:  {props.duration}</h4>
             <h4>More: {props.more}</h4>
+            <h5></h5>
+            <h6></h6>
         </div>
     );
 };
